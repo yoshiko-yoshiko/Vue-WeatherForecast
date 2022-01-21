6c697204083a6c80b8091968ccc7a76ddacd9aef
Kanei <77195827+zbraa@users.noreply.github.com>
2022-01-22 00:42:40 +0900

    weather components

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Weather from '@/components/Weather'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Weather',
+      component: Weather
     }
   ]
 })
